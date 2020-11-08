51b67ea40c3b2fcbee223651639b9e253d95aafe
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-11-08 10:41:31 -0600

    Update fittext.lua

--- fittext.lua
+++ fittext.lua
@@ -1,7 +1,12 @@
 --[[
 	FitText.lua 1.0
-	
-	Copyright 2020, Charlie West-Toebe https://hoidberg.github.io/
+
+	Copyright 2020, Dave Rupert http://daverupert.com 
+	Released under the GNU General Public License
+	https://www.gnu.org/licenses/gpl-3.0.en.html
+	Modified by Charlie West-Toebe https://hoidberg.github.io/
+
+`	Date: Nov 8, 2020, 10:20 AM CST
 ]]
 
 local Player = game.Players.LocalPlayer
@@ -31,16 +36,16 @@ function text.fitText(el, kompressor, options)
 		["minFontSize"] = 1,
 		["maxFontSize"] = 100
 	}, options)
-	
+
 	local function fit()
 		local compressor = kompressor or 1
-		
+
 		local function resizer()
 			el.TextSize = math.clamp((el.Size.X.Scale * screenSizeContainer.AbsoluteSize.X) / (compressor * 10), settings.minFontSize, settings.maxFontSize)
 		end
-		
+
 		resizer()
-		
+
 		local resize = screenSizeContainer:GetPropertyChangedSignal("AbsoluteSize")
 		local textchange = el:GetPropertyChangedSignal("Text")
 		local orientationchange = Player.PlayerGui:GetPropertyChangedSignal("CurrentScreenOrientation")
@@ -49,9 +54,9 @@ function text.fitText(el, kompressor, options)
 		textchange:Connect(resizer)
 		orientationchange:Connect(resizer)
 	end
-	
+
 	fit(el)
-	
+
 	return el
 end
 
